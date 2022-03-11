834c47a03b6ef3e738ae133fc786e013526dc096
borikokeny <bori@minerali.hu>
2022-03-11 11:15:47 +0100

    homework

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// question 1
+
+// var maxHeight = 178;
+// var onBoard = true;
+// var nickName = "Koksi";
+
+
+// question 2
+
+// var firstName = "Bori";
+// var lastName = "Kokeny";
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+
+// question 3
+
+// var animal = "frog";
+// var typeOfFrogIs = typeof animal;
+// console.log("Type of frog is:", typeOfFrogIs);
+
+
+// question 4
+
+// var orderHasShipped = true;
+// if (orderHasShipped===true) {
+//    console.log("The order shipped");
+// } else {
+//    console.log("The order did not ship");
+// }
+
+
+// question 5
+
+// for (var i = 7; i <= 13; i++) {
+//    console.log(i);
+// }
+
+
+
